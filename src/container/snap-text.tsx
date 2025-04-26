6bd097f7683041a497514b8ccad89b970ead72f8
mohammedrakib944 <mohammedrakib944.com>
2025-04-26 23:26:03 +0600

    nothing

---
+++ src/container/snap-text.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SnapTest = () => {
+  return (
+    <div className="h-screen">
+      <div className="h-screen snap-this-section flex items-center justify-center text-6xl bg-amber-300">
+        <h2>Snap Test</h2>
+      </div>
+    </div>
+  );
+};
+
+export default SnapTest;
--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,7 +1,18 @@
+import { cn } from "@/utils/cn";
 import React from "react";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="max-w-[1024px] px-4 mx-auto">{children}</div>;
+const Layout = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("max-w-[1024px] px-4 mx-auto", className)}>
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
